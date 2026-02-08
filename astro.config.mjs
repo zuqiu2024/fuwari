b211fa44e04665b99fd07f0b278ca93f8e7b7a40
zuqiu2024 <zhanxuxiang2022@163.com>
2026-02-09 02:45:04 +0800

    更新 astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import { defineConfig } from 'astro/config'
 import expressiveCode from 'astro-expressive-code'
 import cloudflare from '@astrojs/cloudflare'
 import keystatic from '@keystatic/astro'
+import icon from 'astro-icon' // 补齐这个
 
 // https://astro.build/config
 export default defineConfig({
@@ -17,6 +18,8 @@ export default defineConfig({
     platformProxy: {
       enabled: true,
     },
+    // 添加 kv 绑定支持
+    runtime: { mode: 'complete', binding: 'SESSION' }
   }),
   integrations: [
     tailwind({
@@ -26,13 +29,18 @@ export default defineConfig({
     sitemap(),
     swup(),
     expressiveCode(),
+    icon(), // 必须加上这个，解决 virtual:astro-icon 报错
     keystatic(),
   ],
   vite: {
     build: {
       rollupOptions: {
+        // 确保这些 Node 模块在 Cloudflare 环境不报错
         external: ['node:path', 'node:async_hooks', 'stream', 'util'],
       },
     },
+    ssr: {
+        external: ['node:path', 'node:async_hooks', 'stream', 'util']
+    }
   },
 })
