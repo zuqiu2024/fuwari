1ec396d3bb64471d680c5def38a7e47d4833619b
zuqiu2024 <zhanxuxiang2022@163.com>
2026-02-09 04:16:57 +0800

    更新 astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,18 +6,10 @@ import { defineConfig } from "astro/config";
 import expressiveCode from "astro-expressive-code";
 import icon from "astro-icon";
 import rehypeAutolinkHeadings from "rehype-autolink-headings";
-import rehypeComponents from "rehype-components";
-import rehypeKatex from "rehype-katex";
 import rehypeSlug from "rehype-slug";
 import remarkDirective from "remark-directive";
-import remarkDirectiveRehype from "remark-directive-rehype";
-import remarkGithubAdmonitionsToDirectives from "remark-github-admonitions-to-directives";
 import remarkMath from "remark-math";
 import remarkSectionize from "remark-sectionize";
-
-// 使用这种导入方式绕过构建环境的路径解析问题
-import { admsConfig } from "./src/plugins/rehype-adms-config.mjs";
-import { parseComments } from "./src/plugins/remark-parse-comments.mjs";
 
 export default defineConfig({
   site: "https://zhanxx81201.dpdns.org",
@@ -25,7 +17,7 @@ export default defineConfig({
   trailingSlash: "always",
   integrations: [
     tailwind({
-      nesting: true,
+      applyBaseStyles: false,
     }),
     swup({
       theme: "fade",
@@ -54,15 +46,10 @@ export default defineConfig({
       remarkMath,
       remarkDirective,
       remarkSectionize,
-      parseComments,
-      remarkDirectiveRehype,
-      remarkGithubAdmonitionsToDirectives,
     ],
     rehypePlugins: [
-      rehypeKatex,
       rehypeSlug,
       [rehypeAutolinkHeadings, { behavior: "append" }],
-      [rehypeComponents, admsConfig],
     ],
   },
   vite: {
@@ -70,7 +57,7 @@ export default defineConfig({
       cssMinify: "lightningcss",
     },
     ssr: {
-      noExternal: ["overlayscrollbars", "shiki", "@swup/astro", "flexsearch"],
+      noExternal: ["overlayscrollbars", "shiki", "@swup/astro"],
     },
   },
 });
